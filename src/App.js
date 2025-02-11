86acb1848c82d3d90faf4f6f15718d4de3070ae8
Agustin <agustin.diagnosticar@gmail.com>
2025-02-11 10:45:27 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ class App extends Component {
         dicomWeb: [
           {
             name: 'DCM4CHEE',
-            wadoUriRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/wado',
-            qidoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
-            wadoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
+            wadoUriRoot: 'http://192.168.1.115:8080/dcm4chee-arc/aets/DCM4CHEE/wado',
+            qidoRoot: 'http://192.168.1.115:8080/dcm4chee-arc/aets/DCM4CHEE/rs',
+            wadoRoot: 'http://192.168.1.115:8080/dcm4chee-arc/aets/DCM4CHEE/rs',
             qidoSupportsIncludeField: true,
             imageRendering: 'wadors',
             thumbnailRendering: 'wadors',            
