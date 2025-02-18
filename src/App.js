a415c05adb5043db4f776e8509b69fb766f9c02a
Agustin <agustin.diagnosticar@gmail.com>
2025-02-18 08:48:28 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Viewer } from "@ohif/viewer";
+
 
 import { installViewer } from '@ohif/viewer';
 //import {OHIFExtDicomMicroscopy} from '@ohif/extension-dicom-microscopy'
@@ -15,9 +17,9 @@ class App extends Component {
         dicomWeb: [
           {
             name: 'DCM4CHEE',
-            wadoUriRoot: 'http://192.168.1.115:8080/dcm4chee-arc/aets/DCM4CHEE/wado',
-            qidoRoot: 'http://192.168.1.115:8080/dcm4chee-arc/aets/DCM4CHEE/rs',
-            wadoRoot: 'http://192.168.1.115:8080/dcm4chee-arc/aets/DCM4CHEE/rs',
+            wadoUriRoot: 'http://192.168.1.114:3001/dcm4chee-arc/aets/DCM4CHEE/wado',
+            qidoRoot: 'http://192.168.1.114:3001/dcm4chee-arc/aets/DCM4CHEE/rs',
+            wadoRoot: 'http://192.168.1.114:3001/dcm4chee-arc/aets/DCM4CHEE/rs',
             qidoSupportsIncludeField: true,
             imageRendering: 'wadors',
             thumbnailRendering: 'wadors',            
@@ -28,7 +30,6 @@ class App extends Component {
       } 
     }; 
   
-
   this.ohifViewerConfig = window.config; // or set it here
   this.containerId = 'ohif';
 }
@@ -44,6 +45,7 @@ class App extends Component {
         this.containerId,
         this.componentRenderedOrUpdatedCallback
       );
+      document.getElementsByClassName("cornerstone-canvas").length > 0 ? document.getElementsByClassName("cornerstone-canvas")[0].height = 1200 : null
   }
 
   render () { 
